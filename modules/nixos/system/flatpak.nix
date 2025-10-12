cb06f7d9b7abc9e30f8676c695837d0680adca6d
robin <comfysage@isabelroses.com>
2025-10-12 12:21:58 +0200

    nixos: enable flatpaks

--- modules/nixos/system/flatpak.nix
+++ modules/nixos/system/flatpak.nix
@@ -1,5 +1,6 @@
 {
   lib,
+  pkgs,
   config,
   ...
 }:
@@ -8,7 +9,11 @@ let
 in
 {
   config = mkIf config.garden.profiles.graphical.enable {
-    services.flatpak.enable = false;
+    services.flatpak.enable = true;
+
+    garden.packages = {
+      inherit (pkgs) flatpak;
+    };
 
     environment.sessionVariables.XDG_DATA_DIRS = [ "/var/lib/flatpak/exports/share" ];
   };
